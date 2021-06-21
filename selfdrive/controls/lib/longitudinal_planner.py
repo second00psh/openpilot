7df46bfa4e6cae289d56ce99f30df44c5e85e765
hanabi95 <youngsoo.cha@gmail.com>
2021-06-21 20:49:01 +0900

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -24,8 +24,8 @@ _A_CRUISE_MIN_BP = [  0.,  5.,  10., 20.,  40.]
 
 # need fast accel at very low speed for stop and go
 # make sure these accelerations are smaller than mpc limits
-_A_CRUISE_MAX_V = [1.2, 1., .8, .4, .2]
-_A_CRUISE_MAX_V_FOLLOWING = [1.2, 1., .8, .4, .3]
+_A_CRUISE_MAX_V = [1.2, 1.2, .8, .4, .2]
+_A_CRUISE_MAX_V_FOLLOWING = [1.2, 1.2, .8, .4, .3]
 _A_CRUISE_MAX_BP = [0., 5., 10., 20., 41.]
 
 # Lookup table for turns
